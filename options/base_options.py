cf91699283982e25402e0816af8867535fa80c07
wangmeng9218 <52639477+wangmeng9218@users.noreply.github.com>
2022-09-17 12:50:29 +0800

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -9,15 +9,15 @@ class BaseOptions():
 
     def initialize(self):
         # experiment specifics
-        self.parser.add_argument('--data', default="../ForShared/Datasets")
+        self.parser.add_argument('--data', default="./Datasets")
         self.parser.add_argument('--dataset', default="OCT")
         self.parser.add_argument('--dataset_Super', default="OCT_Super")
         self.parser.add_argument('--num_epochs', type=int, default=100)
         self.parser.add_argument('--log_dirs', type=str, default='./Log')
         self.parser.add_argument('--crop_height', type=int, default=512)
         self.parser.add_argument('--crop_width', type=int, default=256)
-        self.parser.add_argument('--batch_size', type=int, default=4)
-        self.parser.add_argument('--labeled_bs', type=int, default=2)
+        self.parser.add_argument('--batch_size', type=int, default=8)
+        self.parser.add_argument('--labeled_bs', type=int, default=4)
         self.parser.add_argument('--num_threads', default=4, type=int)
         self.parser.add_argument('--net_work', type=str, default='Capsule')
         self.parser.add_argument('--input_ch', type=int, default=1)
@@ -27,20 +27,26 @@ class BaseOptions():
         self.parser.add_argument('--lr', type=float, default=0.0002)
         self.parser.add_argument('--use_gpu', type=bool, default=True)
         self.parser.add_argument('--gpu_ids', type=str, default='0')
-        self.parser.add_argument('--checkpoints_dir', type=str, default='./Semi_Fused_Results_Cu10.2_Github_Super5_BN_bs8_ls4')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='./Semi_Fused_Results')
         self.parser.add_argument('--netG', type=str, default='Capsule')
         self.parser.add_argument('--cuda', default='0')
         self.parser.add_argument('--seed', type=int, default=1337, help='random seed')
 
+        self.parser.add_argument('--model', type=str, default='pix2pixHD')
         self.parser.add_argument('--L1_Loss', type=bool, default=True)
         self.parser.add_argument('--SSIM_Loss', type=bool, default=True)
 
 
         # for displays
+        self.parser.add_argument('--display_winsize', type=int, default=512)
         self.parser.add_argument('--tf_log', action='store_true')
+        self.parser.add_argument('--name', type=str, default='OCT_Denoise')
+
         self.parser.add_argument('--norm', type=str, default='batch')
+
+
         self.parser.add_argument('--no_html', action='store_true')
-        self.parser.add_argument('--num_D', type=int, default=1)
+        self.parser.add_argument('--num_D', type=int, default=2)
         self.parser.add_argument('--n_layers_D', type=int, default=3)
         self.parser.add_argument('--ndf', type=int, default=64)
         self.parser.add_argument('--no_ganFeat_loss', action='store_true')
